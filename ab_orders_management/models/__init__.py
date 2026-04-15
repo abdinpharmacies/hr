721c75149b29c8fcc25db96f42729bc3de0e39c4
root <mohamed.fawzy.dev87@gmail.com>
2026-04-15 13:31:42 +0200

    remove: delete ab_orders_management module from ab_request_managment branch (2)

--- ab_orders_management/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import pharmacy_delivery_branch
-from . import pharmacy_delivery_pilot
-from . import pharmacy_delivery_assignment
--- ab_orders_management/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import test_pharmacy_delivery_management
