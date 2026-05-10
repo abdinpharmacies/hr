05185b0c572b3b68b4a57a538204a99127d562bd
emadco88 <emadco88@gmail.com>
2026-05-10 10:33:27 +0300

    ab_stock_recycling/ NEW from odoo 15.0
    (cherry picked from commit cc0fbe06fe71e8aa06d6b1232684568b2127bc10)
    (cherry picked from commit 21d085ec3743e4ed78072c1366538419943c8e83)
    (cherry picked from commit c119ea1786d6f6de92033165ed553f66b54da9a2)

---
+++ ab_stock_recycling/models/__init__.py
@@ -0,0 +1,6 @@
+﻿from . import ab_stock_recycling_header
+from . import ab_stock_recycling_line
+from . import ab_stock_recycling_need
+from . import ab_stock_recycling_dist
+from . import ab_stock_recycling_excluded_item
+from . import ab_stock_recycling_to_cycle
