4fed3a83227a563e20fb58deb61feb8592abfd4e
Mohamed Fawzy <mohamed.fawzy.dev87@gmail.com>
2026-05-20 13:46:16 +0300

    ab_website_sale_product/fix: make a limit on the user to restrict number of items can buy
    (cherry picked from commit 8964f689b10d5ef70b641f2d13ad97a180d1368b)

--- ab_website_sale_product/models/__init__.py
+++ ab_website_sale_product/models/__init__.py
@@ -1,4 +1,5 @@
 from . import ab_product
 from . import eplus_stock_snapshot
 from . import product_template
+from . import sale_order
 from . import sale_order_line
--- ab_website_sale_product/tests/test_ab_website_sale_product.py
+++ ab_website_sale_product/tests/test_ab_website_sale_product.py
@@ -35,3 +35,40 @@ class TestAbWebsiteSaleProduct(TransactionCase):
         self.assertEqual(template.product_variant_id.barcode, "ABWEBTEST001")
         self.assertEqual(template.public_categ_ids.name, "Website Test Group")
         self.assertEqual(template.product_tag_ids.name, "Website Test Tag")
+
+    def test_ecommerce_cart_quantity_is_limited_by_eplus_snapshot_total(self):
+        card = self.env["ab_product_card"].create({
+            "name": "Website Limited Product",
+        })
+        ab_product = self.env["ab_product"].create({
+            "product_card_id": card.id,
+            "code": "AB-WEB-LIMIT-001",
+            "default_price": 15.0,
+            "default_cost": 5.0,
+            "website_sale_available": True,
+        })
+        self.env["ab_eplus_stock_snapshot"].create({
+            "itm_id": 900001,
+            "itm_code": "AB-WEB-LIMIT-001",
+            "itm_qty": 3.0,
+            "product_id": ab_product.id,
+            "matched_by": "code",
+            "last_sync_date": fields.Datetime.now(),
+        })
+        template = ab_product._sync_website_products()
+        variant = template.product_variant_id
+        order = self.env["sale.order"].create({
+            "partner_id": self.env.ref("base.public_partner").id,
+            "pricelist_id": self.env.ref("product.list0").id,
+            "website_id": self.env["website"].get_current_website().id,
+        })
+
+        qty, warning = order._verify_updated_quantity(
+            self.env["sale.order.line"],
+            variant.id,
+            5.0,
+            variant.uom_id.id,
+        )
+
+        self.assertEqual(qty, 3.0)
+        self.assertIn("You cannot buy more than", warning)
