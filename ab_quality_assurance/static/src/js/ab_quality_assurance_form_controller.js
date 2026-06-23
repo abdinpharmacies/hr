c7133f48ab88e561f03f60fc6fc1134087326c95
hagerYasser <hagerTest@gmail.com>
2026-06-23 16:02:33 +0300

    ab_quality_assurance/FEAT: Add QA notifications, manager replies, and security
    (cherry picked from commit 1cf99cd8d9c6e7a1d69f5a12bb8be7da7f126c08)

--- ab_quality_assurance/static/src/js/ab_quality_assurance_form_controller.js
+++ ab_quality_assurance/static/src/js/ab_quality_assurance_form_controller.js
@@ -125,7 +125,7 @@ patch(FormController.prototype, {
             body: buildScoreSummary(this.model.root),
             confirmLabel: _t("Submit Visit"),
             confirm: async () => {
-                const saved = await this.save({ reload: false });
+                const saved = await this.save();
                 if (!saved || !this.model.root.resId) {
                     return false;
                 }
