cc2a6a2c12744b16c7e6a4032c20a7eaa47aa448
hager yasser <hageryasser2002@gmail.com>
2026-07-14 14:02:32 +0300

    ab_internal_shipment_tracking/FIX: Edit security rules that related with awaiting_warehouse_receipt And Edit UI
    (cherry picked from commit c772b12fcdec73d730c304deeeb4c49fe34907a5)

--- ab_internal_shipment_tracking/models/internal_shipment_history.py
+++ ab_internal_shipment_tracking/models/internal_shipment_history.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class InternalShipmentHistory(models.Model):
@@ -45,3 +46,46 @@ class InternalShipmentHistory(models.Model):
     from_holder_display = fields.Char(readonly=True)
     to_holder_display = fields.Char(readonly=True)
     notes = fields.Text()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        self._check_followup_notes_not_closed(vals_list)
+        return super().create(vals_list)
+
+    def write(self, vals):
+        self._check_followup_notes_not_closed(vals, records=self)
+        return super().write(vals)
+
+    def unlink(self):
+        self._check_followup_notes_not_closed({}, records=self)
+        return super().unlink()
+
+    @api.model
+    def _check_followup_notes_not_closed(self, vals, records=None):
+        vals_list = vals if isinstance(vals, list) else [vals]
+        if records:
+            vals_list = vals_list * len(records)
+            record_values = zip(records, vals_list)
+        else:
+            record_values = ((self.browse(), item) for item in vals_list)
+
+        shipment_ids = {
+            item.get("shipment_id")
+            for _, item in record_values
+            if item.get("shipment_id")
+        }
+        shipments = self.env["ab_internal_shipment"].browse(shipment_ids)
+        shipment_by_id = {shipment.id: shipment for shipment in shipments}
+
+        if records:
+            record_values = zip(records, vals_list)
+        else:
+            record_values = ((self.browse(), item) for item in vals_list)
+
+        for record, item in record_values:
+            action = item["action"] if "action" in item else record.action
+            shipment = shipment_by_id.get(item.get("shipment_id"))
+            if not shipment:
+                shipment = record.shipment_id
+            if action == "note" and shipment.state == "closed":
+                raise UserError(_("Follow-up notes cannot be changed after the shipment is closed."))
