22c61c9ac5a3c46c5421fa4fe1025f3ecb718b63
itharrefaat5 <itharrefaat5@gmail.com>
2026-07-14 14:27:26 +0300

    ab_manpower_need/add manpower need security group

--- ab_manpower_need/__manifest__.py
+++ ab_manpower_need/__manifest__.py
@@ -7,10 +7,10 @@
     'developer': 'Alhassan Hossny',
     'summary': 'Manpower need per hour planning',
     'depends': ['ab_hr'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/manpower_hour_need_views.xml',
-    ],
+    'data': ['security/security_groups.xml',
+             'security/ir.model.access.csv',
+             'views/manpower_hour_need_views.xml',
+             ],
     'assets': {
         'web.assets_backend': [
             'ab_manpower_need/static/src/scss/manpower_hour_need_dashboard.scss',
