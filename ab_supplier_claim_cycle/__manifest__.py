9e78926a01fae50976663f1805fe4320f12c093b
Mohamed Fawzy <mohamed.fawzy.dev87@gmail.com>
2026-07-14 12:54:42 +0300

    ab_supplier_claim_cycle/feat: add Telegram bot integration for supplier claim manager notifications

--- ab_supplier_claim_cycle/__manifest__.py
+++ ab_supplier_claim_cycle/__manifest__.py
@@ -2,11 +2,11 @@
     'name': 'Supplier Claim Cycle',
     'version': '19.0.1.1.0',
     'license': 'LGPL-3',
-    'category': 'AbdinSupplyChain',
+    'category': 'AbdinClaimCycle',
     'author': 'Abdin Pharmacies',
-    'developer': 'Alhassan Hossny',
+    'developer': 'Mohamed Fawzy',
     'application': True,
-    'depends': ['base', 'mail', 'ab_supplier', 'ab_costcenter'],
+    'depends': ['base', 'mail', 'ab_supplier', 'ab_costcenter', 'ab_hr'],
 
     'data': [
         'security/groups.xml',
@@ -14,16 +14,22 @@
         'security/record_rules.xml',
         'data/sequence.xml',
         'data/cron_escalation.xml',
-        'views/menus.xml',
+        'data/cron_telegram_updates.xml',
+        'data/auto_import_managers.xml',
         'views/ab_supplier_claim_escalation.xml',
         'views/ab_supplier_claim_cycle.xml',
         'views/ab_supplier_claim_issue.xml',
+        'views/ab_supplier_claim_telegram_registration_views.xml',
+        'views/menus.xml',
+        'views/telegram_managers_action.xml',
     ],
 
     'installable': True,
     'assets': {
         'web.assets_backend': [
             'ab_supplier_claim_cycle/static/src/scss/supplier_claim_cycle.scss',
+            'ab_supplier_claim_cycle/static/src/js/telegram_managers.js',
+            'ab_supplier_claim_cycle/static/src/xml/telegram_managers.xml',
         ],
     },
 }
