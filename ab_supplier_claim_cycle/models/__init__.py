77f3d0239211cf6b5eeccea1ba68df100b9a8ce0
Mohamed Fawzy <mohamed.fawzy.dev87@gmail.com>
2026-07-14 12:54:42 +0300

    ab_supplier_claim_cycle/feat: add Telegram bot integration for supplier claim manager notifications
    (cherry picked from commit 9e78926a01fae50976663f1805fe4320f12c093b)

--- ab_supplier_claim_cycle/models/__init__.py
+++ ab_supplier_claim_cycle/models/__init__.py
@@ -1,3 +1,5 @@
+from . import ab_supplier_claim_telegram_registration
+from . import ab_supplier_claim_manager
 from . import ab_supplier_claim_cycle
 from . import ab_supplier_stage_history
 from . import ab_supplier_claim_escalation
@@ -6,4 +8,6 @@ from . import ab_claim_error_wizard
 from . import ab_supplier_claim_issue
 from . import ab_costcenter_inherit
 from . import ab_supplier_type_wizard
+from . import ab_res_groups
+from . import ab_delegate_phone
 
