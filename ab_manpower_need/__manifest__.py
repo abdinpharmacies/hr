7fcbcf5490b419a04cf2fa216e18ecca50128330
Hossam Elsheikh <hossam.m.elsheikh@gmail.com>
2026-07-14 11:36:22 +0300

    ab_manpower_need/init module

---
+++ ab_manpower_need/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Manpower Need',
+    'version': '19.0.1.0.0',
+    'license': 'LGPL-3',
+    'category': 'Abdin',
+    'author': 'Abdin Pharmacies',
+    'developer': 'Alhassan Hossny',
+    'summary': 'Manpower need per hour planning',
+    'depends': ['ab_hr'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/manpower_hour_need_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'ab_manpower_need/static/src/scss/manpower_hour_need_dashboard.scss',
+        ],
+    },
+    'application': False,
+    'installable': True,
+}
---
+++ ab_manpower_need/models/__init__.py
@@ -0,0 +1,1 @@
+from . import manpower_hour_need
