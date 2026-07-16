4ebd28805f7951b62f44c661e684a1ca3e8ae5d5
Alhassan Hossny <alhassan.hossny@gmail.com>
2026-07-16 16:27:28 +0300

    ab_hr_telegram_employee_link/refactor: own payroll Telegram integration bridge
    (cherry picked from commit 26d088d50dcd8f4d4e4827cdfad40f3322525a48)
    (cherry picked from commit e6b676f6343c9508d658c8fcf36eba997398c617)

--- ab_hr_telegram_employee_link/__manifest__.py
+++ ab_hr_telegram_employee_link/__manifest__.py
@@ -2,13 +2,16 @@
 {
     "name": "HR Telegram Employee Link",
     "summary": "Link Telegram chat IDs to HR employees by employee code",
-    "version": "19.0.1.1.0",
+    "version": "19.0.1.1.1",
     "license": "LGPL-3",
     "author": "Abdin Pharmacies",
     "developer": "Alhassan Hossny",
     "category": "Human Resources",
-    "depends": ["ab_hr", "ab_telegram_webhook"],
-    "data": [],
+    "depends": ["ab_payroll", "ab_telegram_webhook"],
+    "external_dependencies": {"python": ["telebot"]},
+    "data": [
+        "views/ab_hr_employee_views.xml",
+    ],
     "installable": True,
     "application": False,
 }
--- ab_hr_telegram_employee_link/models/__init__.py
+++ ab_hr_telegram_employee_link/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import ab_hr_employee
+from . import payroll_telegram_service
 from . import telegram_webhook
