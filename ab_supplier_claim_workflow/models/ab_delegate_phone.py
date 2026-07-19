0ab7015172f70e493372bc6669bc641679705cb3
Mohamed Fawzy <mohamed.fawzy.dev87@gmail.com>
2026-07-19 09:52:17 +0300

    ab_supplier_claim_workflow/feat: create supplier claim workflow extension as a separate module from ab_supplier_claim_cycle
    (cherry picked from commit 519bc979aa37c376d108e8219cc98611c33eb56a)

---
+++ ab_supplier_claim_workflow/models/ab_delegate_phone.py
@@ -0,0 +1,37 @@
+from odoo import api, models, fields
+
+
+class AbDelegatePhone(models.Model):
+    _name = 'ab.delegate.phone'
+    _description = 'Delegate Phone Number'
+    _rec_name = 'name'
+
+    name = fields.Char(string='Phone Number', required=True)
+    partner_id = fields.Many2one('ab_costcenter', string='Supplier')
+    is_default = fields.Boolean(string='Default')
+
+    @api.model
+    def name_create(self, name):
+        partner_id = self.env.context.get('default_partner_id')
+        new = self.create({'name': name, 'partner_id': partner_id})
+        return (new.id, new.display_name)
+
+    def write(self, vals):
+        if vals.get('is_default'):
+            for rec in self:
+                if rec.partner_id:
+                    self.search([
+                        ('partner_id', '=', rec.partner_id.id),
+                        ('is_default', '=', True),
+                        ('id', '!=', rec.id),
+                    ]).write({'is_default': False})
+        return super().write(vals)
+
+    def unlink(self):
+        if self.ids:
+            self.env.cr.execute(
+                "DELETE FROM claim_delegate_phone_rel "
+                "WHERE ab_delegate_phone_id = ANY(%s)",
+                [list(self.ids)]
+            )
+        return super().unlink()
