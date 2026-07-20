12c3c8812b7e1cd30aa237dc9379ebd42bf51c4f
itharrefaat5 <itharrefaat5@gmail.com>
2026-07-20 11:57:42 +0300

    ab_request_telegram/separated from a18f20dd955488bd5087b6282329dbd18f9e09c5

--- ab_request_telegram/__manifest__.py
+++ ab_request_telegram/__manifest__.py
@@ -4,7 +4,7 @@
     "license": "LGPL-3",
     "category": "AbdinSupplyChain",
     "application": False,
-    "depends": ["ab_request_management"],
+    "depends": ["ab_request_management", "ab_payroll"],
     "data": [
         "security/ir.model.access.csv",
         "views/ab_hr_bot_views.xml",
