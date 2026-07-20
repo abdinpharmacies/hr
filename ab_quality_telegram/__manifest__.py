a1c68e1a89943ff13a6b02e7581de8d486a74ec3
hager yasser <hageryasser2002@gmail.com>
2026-07-20 13:06:12 +0300

    ab_quality_telegram/FEAT:  Adding telgram feature(part1)
    (cherry picked from commit 09086b44302360689f8cb62c7c67cce28e1f4efe)

---
+++ ab_quality_telegram/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Quality Assurance Telegram Notifications",
+    "version": "19.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Operations",
+    "author": "Abdin Pharmacies",
+    "developer": "Alhassan Hossny",
+    "application": False,
+    "depends": ["ab_quality_assurance", "ab_telegram_webhook"],
+    "data": [],
+    "installable": True,
+}
