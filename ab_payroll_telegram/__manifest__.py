2f5af2ec389dd11c81eb8237ce2a4ec9a6599ae1
Alhassan Hossny <alhassan.hossny@gmail.com>
2026-07-27 10:36:00 +0300

    ab_payroll_telegram/Refactor:changing the module name to be simple
    (cherry picked from commit 91005cee3c4b6ee7638dae105f66554a1ccf8637)

---
+++ ab_payroll_telegram/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Abdin Payroll Telegram",
+    "summary": "Enable payroll Telegram delivery through the central bot service",
+    "version": "19.0.1.4.0",
+    "license": "LGPL-3",
+    "author": "Abdin Pharmacies",
+    "developer": "Alhassan Hossny",
+    "category": "Human Resources/Payroll",
+    "depends": ["ab_payroll", "ab_telegram_bot"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
