097f96823252d44c4cb5d2355df8a3975b597218
itharrefaat5 <itharrefaat5@gmail.com>
2026-09-22 12:25:12 +0300

    ab_supplier/ FIX remove requierd fields

--- ab_supplier/models/ab_supplier_bracket.py
+++ ab_supplier/models/ab_supplier_bracket.py
@@ -16,10 +16,10 @@ class AbSupplierBracket(models.Model):
 
     supplier_id = fields.Many2one('ab_costcenter', index=True)
     start_day = fields.Integer(default=0)
-    termination_day = fields.Integer(default=0, required=True, index=True)
+    termination_day = fields.Integer(default=0, index=True)
     credit_days = fields.Integer(index=True)
     discount = fields.Float(digits=(5, 2))
-    withdrawal_bracket = fields.Float(default=0.0, required=True)
+    withdrawal_bracket = fields.Float(default=0.0)
 
     @api.depends('supplier_id.name', 'payment_type_id.name')
     def _compute_display_name(self):
