6901729a4ac24234d9b73b324a6f293f1cd319b0
emadco88 <emadco88@gmail.com>
2026-09-23 15:56:48 +0300

    ab_product_source/ NEED FIX , STILL ODOO15

---
+++ ab_product_source/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Abdin Product Source',
+    'license': 'LGPL-3',
+    'category': 'AbdinSupplyChain',
+    'depends': ['base', 'ab_taxes', 'ab_product'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menus.xml',
+        'views/ab_product_source.xml',
+    ],
+}
